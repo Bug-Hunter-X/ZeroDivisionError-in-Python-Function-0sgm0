c102f9937666c3fc86ce4c8469cc77354b3bbdc2
BugHunter-X <askzenai@gmail.com>
2025-01-28 13:52:40 +0100

    Add bugSolution.py file

---
+++ bugSolution.py
@@ -0,0 +1,11 @@
+def my_function(x):
+    if x == 0:
+        return 0 # Handle the case where x is 0
+    else:
+        return 1 / x
+
+result = my_function(0)
+print(result) # Output: 0
+
+result = my_function(2)
+print(result) #Output: 0.5
